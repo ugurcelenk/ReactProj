564bc32f98fbf3f7244babcc2254fa064f289d0b
guang yang <blasttnt@gmail.com>
2017-05-05 20:28:02 +0800

    encapsulated_component_test

--- react-proj/src/index.js
+++ react-proj/src/index.js
@@ -3,35 +3,38 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-/*ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <div>root2 test</div>,
-  document.getElementById('root2')
-);
+// ReactDOM.render(
+//   <div>root2 test</div>,
+//   document.getElementById('root2')
+// );
 
 var testArg = 1;
-var element = (
-  <div>
-    <h1>Hello, world!</h1>
-    <h2>It is {testArg}.</h2>
-  </div>
-);
+var Element = function (props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.testProp}.</h2>
+    </div>
+  )
+};
 
 function tick() {
   testArg++;
-  console.log(testArg);
+  // console.log(testArg);
   ReactDOM.render(
-    element,
+    <Element testProp={testArg} />,
     document.getElementById('root')
   );
-  setInterval(tick, 1000);
-}*/
+}
 
-function Welcome(props) {
+setInterval(tick, 1000);
+
+/*function Welcome(props) {
   console.log(typeof props.age);
   // return <div><h1>Hello, {props.person.name}</h1><div>{props.person.age}</div></div>;
   return <div><h1>Hello, {props.person.name}</h1><div>{props.age}</div></div>;
@@ -49,12 +52,12 @@ function Shell() {
       <Welcome person={tempProp} age={"18"} />
       <Welcome person={tempProp} age={"17"} />
     </div>)
-}
+}*/
 
 // console.log("222");
 // const element = <Welcome person={tempProp} age={"19"} />;
 // console.log("333");
-ReactDOM.render(
-  <Shell />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Shell />,
+//   document.getElementById('root')
+// );
