009f636acda21da3689bfa233defa0b9431ff372
guang yang <blasttnt@gmail.com>
2017-05-05 19:49:29 +0800

    test_react_render_function

--- react-proj/src/index.js
+++ react-proj/src/index.js
@@ -12,3 +12,22 @@ ReactDOM.render(
   <div>root2 test</div>,
   document.getElementById('root2')
 );
+
+var testArg = 1;
+var element = (
+  <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {testArg}.</h2>
+  </div>
+);
+
+function tick() {
+  testArg++;
+  console.log(testArg);
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
